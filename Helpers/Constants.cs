55a2cc2af1d52bdbf5d40aa19650275ef1de7802
pauline.aigueperse@etu.unilim.fr <pauline.aigueperse87@orange.fr>
2024-04-14 14:37:52 +0200

    ajout de Models et Helpers

---
+++ Helpers/Constants.cs
@@ -0,0 +1,8 @@
+using System;
+namespace PaulineApp.Helpers
+{
+    public static class Constants
+    {
+        public static string BaseUrl = "https://api.spacexdata.com/v4/";
+    }
+}
---
+++ ViewModels/APIPage.cs
@@ -0,0 +1,41 @@
+using System;
+using PaulineApp.Models;
+using PaulineApp.Helpers;
+using System.Collections.ObjectModel;
+using Newtonsoft.Json;
+
+
+namespace PaulineApp.ViewModels
+{
+
+    public class PastLaunchesViewModel : BaseViewModel
+    {
+        public ObservableCollection<Root> LatestLaunchs { get; set; }
+
+        private readonly HttpClient _client;
+
+        public PastLaunchesViewModel()
+        {
+            _client = new HttpClient();
+            LatestLaunchs = new ObservableCollection<Root>();
+        }
+
+        public void PopulateLatestLaunchs()
+        {
+            foreach (Root lauch in RetriveAllLatestLaunchs())
+            {
+                LatestLaunchs.Add(lauch);
+            }
+        }
+
+        private ObservableCollection<Root> RetriveAllLatestLaunchs()
+        {
+            var latestLaunchsSerialized = _client.GetStringAsync(Constants.BaseUrl + "launches/past").Result;
+            var latestLaunchsDeserialized = JsonConvert.DeserializeObject<List<Root>>(latestLaunchsSerialized);
+
+            ObservableCollection<Root> allLaunchs = new(latestLaunchsDeserialized);
+
+            return allLaunchs;
+        }
+    }
+}
