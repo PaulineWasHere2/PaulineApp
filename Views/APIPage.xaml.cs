c0a803e5feae32e61101517177fbb33c61e13038
pauline.aigueperse@etu.unilim.fr <pauline.aigueperse87@orange.fr>
2024-04-14 16:20:39 +0200

    ajout d'images et changement de la MainPage

--- Views/APIPage.xaml.cs
+++ Views/APIPage.xaml.cs
@@ -3,41 +3,40 @@ using PaulineApp.Models;
 
 namespace PaulineApp.Views;
 
-
-public partial class APIPage : ContentPage
-{
-    private readonly APIPage _viewModel;
-
-    public APIPage()
+    public partial class APIPage : ContentPage
     {
-        InitializeComponent();
+        private readonly APIPage _viewModel;
 
-        _viewModel = new APIPage();
-        BindingContext = _viewModel;
+        public APIPage()
+        {
+            InitializeComponent();
 
-        _viewModel.PopulateLatestLaunchs();
+            _viewModel = new APIPage();
+            BindingContext = _viewModel;
+
+            _viewModel.PopulateLatestLaunchs();
+        }
+
+        private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item is not Root launch || string.IsNullOrEmpty(launch.links.webcast))
+            {
+                await DisplayAlert(string.Empty, "This release does not yet have a video.", "Ok");
+                return;
+            }
+
+            try
+            {
+                Uri uri = new(launch.links.webcast);
+                await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Data);
+
+                await DisplayAlert("An unexpected error occured",
+                    "No browser may be installed on the device",
+                    "Ok");
+            }
+        }
     }
-
-    private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
-    {
-        if (e.Item is not Root launch || string.IsNullOrEmpty(launch.links.webcast))
-        {
-            await DisplayAlert(string.Empty, "This release does not yet have a video.", "Ok");
-            return;
-        }
-
-        try
-        {
-            Uri uri = new(launch.links.webcast);
-            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
-        }
-        catch (Exception ex)
-        {
-            Console.Write(ex.Data);
-
-            await DisplayAlert("An unexpected error occured",
-                "No browser may be installed on the device",
-                "Ok");
-        }
-    }
-}
